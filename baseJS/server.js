e264578276ce01164d16982d576c44f8162a097a
Liam <wildrew@nuevaschool.org>
2020-04-04 16:02:16 -0700

    two player movement
    two player movement finally works seamlessly

--- baseJS/server.js
+++ baseJS/server.js
@@ -1,60 +1,206 @@
+//all the imports for servers and sockets;
 var express = require('express');
 var app = express()
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var path = require('path')
-var connections = [];
-var allPlayers = []
-var counter = 1;
 
+//functionality I want to add:
+//when a key is released, only cancel that key input, not the entire move input.
+//fix whatever the hyperspace thing is
+//defnitely a server side problem
+
+//this is an array for all active socket connections. Contains both user ids and socket ids
+var connections = [];
+//counts how many users are connected to assign user ids.
+var counter = 0;
+
+//game settings determined here and sent to clients
+//clients cant be trusted, so we determine this here
+let gameSettings = {
+  // this stuff is self explanatory
+  canvasWidth: 700,
+  canvasHeight: 700,
+  playerWidth: 25,
+  playerHeight: 25,
+  //the send interval determines how ofter i emit basic game information to all users
+  //this is slowed down for development purposes, but when finalized this will be 16 milliseconds
+  sendInterval: 16 //in milliseconds
+}
+
+//this is the queue object that I emit with the send interval frequency
+//this is all the stuff that changed, and the client needs to render it
+
+
+let queue = {
+  player: [],
+  key: [],
+  action: []
+  // movePlayer: [],
+}
+
+
+
+
+
+//gets username from socket id
+// this makes things easy for the socket functions
+function getNameFromId(id){for (let i = 0; i < connections.length; i++){if (id == connections[i][1]){return connections[i][0]}}}
+
+function keyInList(list, keycode){
+  if (list != undefined){
+    for (let p = 0; p< list.length; p++){
+      //same key cant be pressed twice
+      if (list[p] == keycode){
+        //do nothing
+        return true
+      }
+    }
+
+  }
+  return false
+}
+//gets game.js and html files
+//this is done for local host, not the sockets
+//this is a basic server getting these files on client load
 app.get('/', function(req, res){
   console.log('getting')
-  res.sendFile(__dirname + '/src/other.html');
+  res.sendFile(__dirname + '/src/gamepage.html');
   app.use(express.static(path.join(__dirname, 'src')))
-
-  // res.sendFile(__dirname + '/game.js')
-  // res.send(test)
 });
 
-// io.on('connection', function(socket){
-//   console.log('a player connected');
-//   socket.on('coordinates', function(coords){
-//     io.emit('my coords', coords);
-//     console.log('a player' + coords)
-//   })
-// })
-
+//this io.on function runs whenever a new socket is connected.
 io.on('connection', function(socket){
+  //checking sockets
+  console.log(`new connection ${socket.id}$`)
+  //this emits the game settings to the clients
+  //this is the first thing that runs, just so clients can set up the Canvas
+  socket.emit('getGameSettings', gameSettings)
+
   console.log('USER CONNECTED')
   console.log(connections.length)
-  if (connections.length == 0){
-    connections.push('user0')
-  }
-  else if (connections[0] == 'user0'){
-    connections.push('user'+counter.toString())
-    counter +=1;
-  }
-  socket.emit('sendClientUsername', connections.slice(-1)[0])
-  console.log(connections.slice(-1)[0])
 
+  //I put the socket id in an array with the userId. User id is for the players and game, socket id is for the socket disconnect
+  connections.push(['user'+counter.toString(), socket.id])
+  //adds one to the counter when a new user connects so the next user gets a different id
+  counter +=1
+
+
+
+  //sends the client its own user name
+  socket.emit('sendClientUsername', connections.slice(-1)[0][0])
+  //logs the last user to connect
+  // console.log(connections.slice(-1)[0][0])
+
+  //if there are already players in the game, these player id's are sent to the newly connected player.
   if (connections.length > 1){
     socket.emit('allExistingPlayers', connections)
   }
 
+  //once this new player recieves its player id, the server sends this id to all the other players in the game so they know a next player joined
   socket.on('sendingNewPlayer', function(user){
-    console.log('New Player BroadCasted')
+    console.log('New Player Broadcasted')
     socket.broadcast.emit('broadcastingNewPlayer', user)
   })
-
-  socket.on('playerMoved', function(coords){
-    console.log('message: ' + coords);
-    socket.broadcast.emit('newPlayerCoords', coords);
-  });
+//when a player disconnects, the server removes the player from the player list.
+//the server also emits the username of the disconnected player so the client can stop rendering them
   socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
+    console.log('PLAYER DISCONNECTED')
+    connections.forEach((element, index) => {
+      if (element[1] == socket.id){
+        socket.broadcast.emit('playerDisconnected', connections[index][0]);
+        connections.splice(index, 1);
+        for (let p = 0; p <queue.action.length; p++){
+          if (getNameFromId(socket.id) == queue.action[p]){
+            queue.action.splice(p, 1);
+            queue.key.splice(p, 1)
+          }
+        }
 
+      }
+    })
+  });
+
+
+//this appears to work, so now I need to figure out how to cancel moves
+  socket.on('requestingMove', function(keycode){
+    console.log(getNameFromId(socket.id), 'is requesting to MOVE', keycode)
+
+    //if no moves exist
+    if (queue.action.length == 0){
+      //request move
+      queue.action.push(getNameFromId(socket.id));
+      queue.key.push([keycode])
+      console.log('First Player Acting')
+    }
+
+
+    else if (queue.action.length > 0){
+      //if moves already exist:
+      //search existing moves
+      for (let i = 0; i<queue.action.length; i++){
+        console.log('action length', queue.action.length)
+        //if player name is already in the action queue
+
+        if (getNameFromId(socket.id) == queue.action[i]){
+          console.log('Player is already in  queue')
+          //check existing keys
+          //this for loop searches through the keys list for that particular player
+          if (keyInList(queue.key[i], keycode) == false){
+            console.log('New Key has been pressed')
+            queue.key[i].push(keycode)
+          }
+
+          else if (keyInList(queue.key[i], keycode) == true){
+            console.log('Key is already in queue')
+            //do nothing to avoid having the same function called multiple times
+          }
+        }
+
+      }
+
+      if (queue.action.includes(getNameFromId(socket.id)) == false){
+        // console.log('NEW player acting')
+        queue.action.push(getNameFromId(socket.id));
+        queue.key.push([keycode])
+      }
+
+    }
+
+  })
+
+  socket.on('requestingStop', function(keycode){
+    name = getNameFromId(socket.id)
+    console.log(name, 'is requesting to stop', keycode)
+    for (let i =0; i <queue.action.length; i++){
+      //making sure the player is in the action queue
+      if (name == queue.action[i]){
+        let al = queue.key[i].length
+        //search all keys in the queue
+        for (let k = 0; k < al; k++){
+          if (queue.key[i][k] == keycode){
+            queue.key[i].splice(k, 1)
+            if (queue.key[i].length == 0){
+              queue.key.splice(i, 1)
+              queue.action.splice(i, 1)
+            }
+          }
+        }
+      }
+    }
+  })
+
+  setInterval(()=>{
+    for (let i = 0; i < connections.length; i++){
+      queue.player.push(connections[i][0])
+    }
+    // console.log('interval firing')
+    // console.log('outbound queue', queue)
+    io.emit('sendingData', queue)
+    queue.player = [];
+
+  }, gameSettings.sendInterval);
+});
 
 
 http.listen(8000, function(){
--- baseJS/src/game.js
+++ baseJS/src/game.js
@@ -1,6 +1,3 @@
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext("2d");
-
 class Canvas {
   constructor(ctx) {this.ctx = ctx};
   drawBoard() {
@@ -40,6 +37,8 @@ class Canvas {
 
 }
 
+let myCanvas = new Canvas(ctx);
+
 //at the moment, this doesn't do anything
 //this is extra structure that I don't yet need but might want
 class Component {
@@ -52,19 +51,15 @@ class Component {
     this.color = color}
 }
 
-//Runs stuff
-let myCanvas = new Canvas(ctx);
-myCanvas.drawBoard();
-
 class Player extends Component{
   //lots of stuff will need to be added to this (maybe)
-  constructor(ctx, xcoord, ycoord, dimensions, color, name){
+  constructor(ctx, xcoord, ycoord, dimensions, color, canvas, name){
     super(ctx, name, xcoord, ycoord, color, dimensions);
+    this.time = 0;
+    this.canvas = canvas;
   }
   //for debugging purposes
-  isProperty(){
-    console.log('I am a property that can be called')
-  }
+  isProperty(){console.log('I am a property that can be called')}
 
   moveTo(x, y){
     this.xcoord = x;
@@ -73,25 +68,42 @@ class Player extends Component{
     this.drawPlayer()
     return [this.xcoord, this.ycoord];
   }
-  //move methods
-  moveDirection(isHorizontal, posNeg){
-    var self = this;
-    //isHorizontal for horizontal vs vertical: true = x, false =y
-    //posNeg for direction: 1 is positive (down, right), -1 is negative (up, left). Also determines speed.
-    for (let i=0; i<2; i++){
-      if (isHorizontal===true){this.xcoord=this.xcoord+posNeg}
-      else if (isHorizontal === false){this.ycoord = this.ycoord+posNeg}};
-    myCanvas.drawBoard();
-    for (let p; p< existingPlayers.length; p++){
-      existingPlayers[p].drawPlayer();
-    }
-    this.drawPlayer()
 
-    let raf = window.requestAnimationFrame(function(){self.moveDirection(isHorizontal, posNeg)});
-    window.addEventListener(event="keyup", function(e){window.cancelAnimationFrame(raf)})
-    socket.emit('playerMoved', [myUser, this.xcoord, this.ycoord]);
+  //move methods
+
+  //planning on making a new move method instead of an old one
+  moveDirection(angle){
+    for (let i = 0; i<2; i++){
+      if (angle == 0){this.ycoord -=1}
+      else if (angle == 180){this.ycoord +=1}
+      else if (angle == 90){this.xcoord +=1}
+      else if (angle == 270){this.xcoord -=1}
+    }
+    myCanvas.drawBoard();
+    this.drawPlayer();
     return [this.xcoord, this.ycoord];
   }
+
+  // move2Direction(isHorizontal, posNeg){
+  //   // console.log('function called')
+  //   var self = this;
+  //   //isHorizontal for horizontal vs vertical: true = x, false =y
+  //   //posNeg for direction: 1 is positive (down, right), -1 is negative (up, left). Also determines speed.
+  //
+  //   //time cancels the loop automatically and reruns it
+  //   this.time +=1;
+  //
+  //
+  //   for (let i=0; i<2; i++){
+  //     if (isHorizontal===true){this.xcoord +=posNeg}
+  //     else if (isHorizontal === false){this.ycoord +=posNeg}};
+  //   myCanvas.drawBoard();
+  //   // for (let p; p< existingPlayers.length; p++){
+  //   //   existingPlayers[p].drawPlayer();
+  //   // }
+  //   this.drawPlayer()
+  //   return [this.xcoord, this.ycoord];
+  // }
 
   drawPlayer(){
     ctx.beginPath();
@@ -100,102 +112,181 @@ class Player extends Component{
     ctx.fillRect(this.xcoord, this.ycoord, this.dimensions[0], this.dimensions[1]);
     ctx.stroke();
   }
-}
 
-var myUser = null
-var player1 = null;
-var existingPlayers = []
-let player1Dimensions = [25, 25];
+  requestMove(keycode){
+      socket.emit('requestingMove', keycode)
+  }
 
-function drawAllPlayers(players){
-  for (let i = 0; i<players.length; i++){
-    players[i].drawPlayer();
+  requestStop(keycode){
+    socket.emit('requestingStop', keycode)
   }
 
 }
-//socket stuff
+
+function drawAllPlayers(players){for (let i = 0; i<players.length; i++){players[i].drawPlayer()}}
+
+
+
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext("2d");
+
 var socket = io();
+
+let myUsername;
+let player1;
+let existingPlayers = [];
+let gameSettings;
+let playerDimensions;
+
+let toRender = {
+  player: [],
+  key: [],
+  action: [],
+  angle: []
+  // movePlayer: []
+
+}
+let otherPlayer;
+
+function getPlayerbyName(name){for (let i =0; i< existingPlayers.length; i++){if (name ==existingPlayers[i].name){return existingPlayers[i]}}}
+
+function checkIfMe(user){if (user == myUsername){return true}}
+//all this socket stuff takes care of the protocols when players leave and join
+
+socket.on('getGameSettings', function(input){
+  gameSettings=input;
+  playerDimensions = [gameSettings.playerWidth, gameSettings.playerHeight];
+  // otherPlayer = new Player(ctx, 100, 100, playerDimensions, 'brown', myCanvas, 'testplayer');
+  canvas.width = gameSettings.canvasWidth;
+  canvas.height = gameSettings.canvasHeight;
+  myCanvas.drawBoard();
+})
+
 socket.on('sendClientUsername', function(user){
   console.log(user)
-  myUser = user;
-  player1 = new Player(ctx, 200, 300, player1Dimensions, 'green', myUser[0])
+  myUsername = user;
+  //this player is me
+  existingPlayers.push(new Player(ctx, 200, 300, playerDimensions, 'green', myCanvas, myUsername));
+  player1 = existingPlayers.slice(-1)[0];
   socket.emit('sendingNewPlayer', user);
 })
 
 socket.on('allExistingPlayers', function(connections){
   for (let i=0; i<connections.length; i++){
-    if (connections[i] == myUser){
-      console.log('do nothing, this is me')
-    }
-    else{
-      let newPlayer = new Player(ctx, 600, 600, player1Dimensions, 'black', connections[i])
-      existingPlayers.push(newPlayer);
-    }
+    console.log('connectsion[i]', connections[i])
+    if (connections[i][0] == myUsername){console.log('do nothing, this is me')}
+
+    else{existingPlayers.push(new Player(ctx, 400, 400, playerDimensions, 'black', myCanvas, connections[i][0]))}
   }
-  console.log('all players loaded')
-  console.log(existingPlayers)
+  console.log('ALL EXISTING PLAYERS HAVE BEEN LOADED')
 })
 
 socket.on('broadcastingNewPlayer', function(user){
-  let aNewPlayer = new Player(ctx, 500, 400, player1Dimensions, 'brown', user)
-  existingPlayers.push(aNewPlayer);
-  console.log('new player loaded')
-  console.log(existingPlayers)
+  existingPlayers.push(new Player(ctx, 300, 250, playerDimensions, 'orange', myCanvas, user));
+  console.log('A NEW PLAYER HAS JOINED')
 })
 
-// let otherPlayer = new Player(ctx, 500, 500, player1Dimensions, 'red', 'otherPlayer');
-
-
-socket.on('newPlayerCoords', function(coords){
-  console.log('recieving coords')
-  if (coords[0] == myUser[0]){
-    console.log('my own coords')
-    return true
-  }
-  else {
-    console.log('Another Player Moved')
-    player1.drawPlayer()
-    for (i=0; i<existingPlayers.length; i++){
-      if (coords[0]==existingPlayers[i].name){
-        console.log('made it jere')
-        existingPlayers[i].moveTo(coords[1], coords[2]);
-      }
+socket.on('playerDisconnected', function(user){
+  existingPlayers.forEach((element, index, array) => {
+    if (element.name[0]==user){
+      console.log( `${user} HAS DISCONNECTED`)
+      existingPlayers.splice(index, 1);
     }
-  }
+  })
 })
 
 window.addEventListener(event="keydown", function(e){
+  //idk if i need to worry about repeating
   if (e.repeat == true){return true};
+  let keycode = e.keyCode;
+  player1.requestMove(keycode)
+})
 
-  switch (e.keyCode){
+window.addEventListener(event="keyup", function(e){
+  let keycode = e.keyCode;
+  player1.requestStop(keycode);
+})
+
+socket.on('sendingData', function(queue){
+  //this for loop converts the user id recieved from the server and converts it into its corresponding instance of the player class
+  // toRender.player = [];
+  // for (let i = 0; i<queue.player.length; i++){toRender.player.push(getPlayerbyName(queue.player[i]))}
+
+  // toRender.player.push(getPlayerbyName(queue.player))
+  toRender.key = []
+  toRender.key.push(queue.key)
+  // console.log(toRender.key)
+  // toRender.angle = []
+  // toRender.angle.push(queue.angle);
+  // console.log(toRender.angle)
+
+  toRender.action = [];
+  toRender.action.push(queue.action)
+  // render()
+})
+
+//this function can be configurable depending on if I choose WASD or arrows
+//it gets the move angle from the keycode
+//at the moment, my players can only move one direction at a time
+//I doubt the problem is here, but is possible
+// BUG:
+
+//use this
+function getAngleFromKey(key){
+  switch (key) {
     case 39:
-      let outsideRaf = window.requestAnimationFrame(function(){
-        player1.moveDirection(true, 1)
-        // renderMove(player1);
-      })
+      return 90;
       break;
 
     case 37:
-      window.requestAnimationFrame(function(){player1.moveDirection(true, -1, existingPlayers)});
+      return 270
       break;
 
     case 38:
-      window.requestAnimationFrame(function(){player1.moveDirection(false, -1, existingPlayers)});
+      return 0;
       break;
 
     case 40:
-      window.requestAnimationFrame(function(){player1.moveDirection(false, 1, existingPlayers)});
+      return 180;
       break;
 
+    default:
+      return false
   }
-})
+}
+
+//use this
+
+//needs to get updated
+function animate(){
+  // console.log('animating')
+  if ((toRender.action[0] != undefined)){
+    //if there are any actions to execute
+    if (toRender.action[0][0] != undefined){
+      //parses through actions
+      for (let i = 0; i < toRender.action[0].length; i++){
+
+        let player = getPlayerbyName(toRender.action[0][i])
+        if (player != undefined){
+          player.drawPlayer()
+          for (let k = 0; k<toRender.key[0][i].length; k++){
+            let angle = getAngleFromKey(toRender.key[0][i][k]);
+            player.moveDirection(angle)
+            drawAllPlayers(existingPlayers)
+          }
 
 
-//for debugging purposes only:
-// function renderMove(player){
-//   let coords = player.moveDirection(true, 1)
-//   let raf = window.requestAnimationFrame(function(){renderMove(player)})
-//   window.addEventListener(event="keyup", function(e){
-//     window.cancelAnimationFrame(raf);
-//   })
-// }
+        }
+
+      }
+    }
+  }
+  // toRender.action = []
+  window.requestAnimationFrame(function(){
+    animate()
+  })
+}
+
+//fix above
+
+window.requestAnimationFrame(function(){animate()})
